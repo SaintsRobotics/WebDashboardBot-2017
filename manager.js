def054c5aaad32371effaf9309b1c799a585ff34
Benjamin Yin <Fellfromatree@gmail.com>
2016-10-14 20:30:21 -0700

    Web Code is done. Notice the capitals on Web Code. That is because it is Very Important Web Code. VIWC. Yes.

--- manager.js
+++ manager.js
@@ -1,65 +1,59 @@
 var socket = new WebSocket("ws://" + window.location.hostname + ":1899");
 
 var dictionary;
-$().get('define.json' function(data, status){
+$().get('define.json', function(data, status){
 	dictionary = JSON.parse(data);
-});
+	console.log("dict loaded");
+}, true);
 
-
-socket.onMessage = function(data){
+socket.onmessage = function(data){
 	data = JSON.parse(data.data);
 	if(data.type == "error"){
 		//deal with this later
 		console.log(data);
-	}else if(data.type == "delta"){
-		for(var key in data){
-			if(key == "type"){
-				continue;
-			}
-			if(dictionary[key]){
-				for(var pair in data[key]){
-					if(!dictionary[key][pair] || !dictionary[key][pair]["server-writable"]){
-						socket.send("{'type':'error','message':'You shouldn\\'nt be able to write to that'}");
-						return;
-					}
+		return;
+	}
+	var delta;
+	if(data.type=="delta"){
+		delta=true;
+	}else if(data.type="confirm"){
+		delta=false;
+	}else{
+		error("malformed!");
+		return;
+	}
+	for(var key in data){
+		if(key == "type"){
+			continue;
+		}
+		if(dictionary[key]){
+			for(var pair in data[key]){
+				if(!dictionary[key][pair] || (delta!=dictionary[key][pair]["server-writable"])){
+					error("wrong permissions or doesn't exist!");
+					return;
 				}
-			}else{
-				socket.send("{'type':'error','message':'You shouldn\\'nt be able to write to that'}");
 			}
-		}
-		for(var key in data){
-			if(key == "type"){
-				continue;
-			}
-			for(var pair in key)
-				dictionary[key][pair] = data[key][pair];
-		}
-		data.type = "confirm";
-		socket.send(JSON.stringify(data));
-	}else if(data.type == "confirm"){
-		for(var key in data){
-			for(var key in data){
-			if(key == "type"){
-				continue;
-			}
-			if(dictionary[key]){
-				for(var pair in data[key]){
-					if(!dictionary[key][pair] || dictionary[key][pair]["server-writable"]){
-						socket.send("{'type':'error','message':'I never sent that'}");
-						return;
-					}
-				}
-			}else{
-				socket.send("{'type':'error','message':'I never sent that'}");
-			}
-		}
-		for(var key in data){
-			if(key == "type"){
-				continue;
-			}
-			for(var pair in key)
-				dictionary[key][pair] = data[key][pair];
-		}
+		}else{
+			error("wrong permissions or doesn't exist!");
+			return;
 		}
 	}
+	if(delta){
+		data.type = "confirm";
+		socket.send(JSON.stringify(data));
+	}
+	for(var key in data){
+		if(key == "type"){
+			continue;
+		}
+		for(var pair in data[key])
+			dictionary[key][pair].value = data[key][pair];
+	}
+}
+function testData(message){
+	socket.onmessage({data:JSON.stringify(message)});
+}
+function error(message){
+	console.log("Error: " + message);
+	socket.send(JSON.stringify({type:"error",message:message}));
 }
