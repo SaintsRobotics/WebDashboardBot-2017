04dd22fc10e3a2e5e18467fb058b53e6703151aa
Benjamin <fellfromatree@gmail.com>
2016-11-04 04:38:20 +0000

    prevent partial motor lock

--- src/com/saintsrobotics/webinterfacebot/motors/MotorSet.java
+++ src/com/saintsrobotics/webinterfacebot/motors/MotorSet.java
@@ -1,18 +1,20 @@
+import java.util.reflection;
 public abstract class MotorSet{
     private int[] motors;
     private boolean enabled = false;
     
     public void enable(){
         Field[] fields = motors.getClass().getDeclaredFields();
-        motors.motors = new int[fields.length];
+        int[] motorsTemp = new int[fields.length];
         try{
         	for(int i = 0; i < fields.length; i++){
-                motors.motors[i] = ((Motor)fields[i].getObject(motors)).getPin();
+                motorsTemp[i] = ((Motor)fields[i].getObject(motors)).getPin();
                 Robot.motors.lock(motors.motors[i]);
         	}
         }catch(Exception e){
                 System.out.println("this shouldn't happen");
         }
+        this.motors = motorsTemp;
         motors.enabled = true;
     }
     public void disable(){
