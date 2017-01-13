1c13a5c0f9b463f94c0a2c9f7aaeb881d4157611
t2001s <tara2001s@gmail.com>
2017-01-12 21:25:44 -0800

    included case where the current speed plus the rate doesn't equal the goal exactly

--- src/com/saintsrobotics/webinterfacebot/tasks/Ramping.java
+++ src/com/saintsrobotics/webinterfacebot/tasks/Ramping.java
@@ -9,10 +9,17 @@ public class Ramping extends Task {
 	protected void run() {
 		while(true) {
 			for (Motor motor : Motors.getMotors()) {
-				if (motor.getCurrent() < motor.getGoal() && motor.getRate() < motor.getGoal()) {
-					motor.set(motor.getCurrent() + motor.getRate());
-				} else if (motor.getCurrent() > motor.getGoal() && motor.getRate() > motor.getGoal() && motor.getRate() < motor.getCurrent()) {
-					motor.set(motor.getCurrent() - motor.getRate());
+				double current = motor.getCurrent();
+				double goal = motor.getGoal();
+				double rate = motor.getRate();
+				if ((current + rate) > goal) {
+					motor.set(goal);
+				} else if ((current - rate) < goal) {
+					motor.set(goal);
+				} else if (current < goal) {
+					motor.set(current + rate);
+				} else if (current > goal) {
+					motor.set(current - rate);
 				}
 			}
 			yield (new WaitForFrame());
